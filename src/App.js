135bedc9ba985343e3c3d24806544916d9095dd2
Shekhar.Thakur04 <connectshekharthakur09@gmail.com>
2022-10-30 14:24:04 +0530

    Creating Buttons and Input for results !

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React from "react";
 
 function App() {
+  
   return (
+    <>
     <div className="App">
-        <h1>Calculator</h1>
+        <form>
+          <input type="text" />
+          <button>Clear</button>
+          <button>C</button>
+          <button>&divide;</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>&times;</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>&ndash;</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+
+        </form>
     </div>
+    </>
   );
 }
 
